e61cf5a35db46e0221ed535d072ccb39c2822d4f
Shashank Sharma <shashank.hacky0@gmail.com>
2024-02-19 17:43:35 +0530

    added whatsapp feature

--- js/app.js
+++ js/app.js
@@ -50,3 +50,39 @@ document.addEventListener('DOMContentLoaded', () => {
     navbar.classList.toggle("sticky", window.scrollY > 0);
   })
   
+
+  // whatsapp
+    var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?29174';
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = url;
+    var options = {
+  "enabled":true,
+  "chatButtonSetting":{
+      "backgroundColor":"#4dc247",
+      "ctaText":"",
+      "borderRadius":"25",
+      "marginLeft":"0",
+      "marginBottom":"50",
+      "marginRight":"50",
+      "position":"right"
+  },
+  "brandSetting":{
+      "brandName":"",
+      "brandSubTitle":"",
+      "brandImg":"https://w7.pngwing.com/pngs/405/859/png-transparent-building-cartoon-facade-building-service-condominium-apartment-thumbnail.png",
+      "welcomeText":"Welcome to SmartSignal",
+      "messageText":"",
+      "backgroundColor":"#0a5f54",
+      "ctaText":"Start Chat",
+      "borderRadius":"25",
+      "autoShow":false,
+      "phoneNumber":"+918748967695"
+  }
+};
+    s.onload = function() {
+        CreateWhatsappChatWidget(options);
+    };
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
